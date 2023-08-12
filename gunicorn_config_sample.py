1eb0bdf244d593e491af4573d6519b1e57faf7c2
Miskler <lysenkomiha913@gmail.com>
2023-08-12 03:11:57 +0300

    Update gunicorn_config_sample.py

--- gunicorn_config_sample.py
+++ gunicorn_config_sample.py
@@ -1,4 +1,1 @@
-bind = "0.0.0.0:443"  # Указывает Gunicorn слушать все IP-адреса на порту 443
-certfile = "/etc/letsencrypt/live/YOU_DOMEN.com/fullchain.pem"  # Путь к вашему SSL сертификату
-keyfile = "/etc/letsencrypt/live/YOU_DOMEN.com/privkey.pem"  # Путь к вашему приватному ключу
 workers = 5  # Количество рабочих процессов Gunicorn
